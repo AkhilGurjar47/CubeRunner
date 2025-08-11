676735931fe29bb40ac1eb60f605eb350d0831fd
Akhil Gurjar <akhilgurjarji2508@gmail.com>
2025-08-11 10:52:00 +0530

    add Bullet System

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
@@ -9,6 +10,9 @@ public class PlayerScript : MonoBehaviour
     public float speed = 10f;
     public float maxX=4.45f;
     public float minX=-4.45f;
+    public GameObject bullet;
+    public Transform bulletSpawnPoint;
+    public bool endPoint;
     void Start()
     {
         
@@ -20,15 +24,25 @@ public class PlayerScript : MonoBehaviour
         transform.position = playerPos;
         if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
+            endPoint = true;
             transform.position = transform.position + new Vector3(speed * Time.deltaTime, 0, 0);
         }
         if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             transform.position = transform.position - new Vector3(speed * Time.deltaTime, 0, 0);
         }
+        if(Input.GetKeyDown(KeyCode.S) && endPoint)
+        {
+            Shoot();
+        }
     }
     private void FixedUpdate()
     {
         rigidbody.AddForce(0, 0, force * Time.deltaTime);
+    }
+    private void Shoot()
+    {
+        GameObject Bullet = Instantiate(bullet, bulletSpawnPoint.position, Quaternion.identity);
+        Bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * 20;
     }
 }
